390af6bfb5a0ce7bef1d759e818c5683115691db
ashrafmdgalib <mdgalibashrafbd@gmail.com>
2024-06-22 03:51:12 +0600

    removeall

--- codeforces.cpp
+++ codeforces.cpp
@@ -2,18 +2,5 @@
 using namespace std;
 int main ()
 {
-    int a, b, c, d;
-    cin >> a >> b >> c >> d;
-    if(a>b && b>c && c>d){
-        cout << a << endl;
-    }
-    else if(b>c && b>d && b>a){
-        cout << b << endl;
-    }
-    else if(c>a && c>b && c>d){
-        cout << c << endl;
-    }
-    else{
-        cout << d << endl;
-    }
+    
 }
